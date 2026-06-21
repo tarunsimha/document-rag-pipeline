61826a65ac2d4cb8220f2a6b1f368fe066619b84
Tarun Simha <tarunsimha2509@gmail.com>
2026-06-21 22:55:27 +0530

    Improved the chunker by using nltk

--- chunker.py
+++ chunker.py
@@ -1,12 +1,31 @@
-def chunk_text(text, chunk_size=1000, overlap=200):
-    chunks = []
-    start = 0
-    while start < len(text):
-        end = min(start + chunk_size, len(text))
-        chunks.append(text[start:end])
-        if end == len(text):
-            break
+from nltk.tokenize import sent_tokenize
 
-        start += chunk_size - overlap
+def chunk_text(text, chunk_size=1000, overlap_sentences=2):
+    sentences = sent_tokenize(text)
+
+    chunks = []
+    current_chunk = []
+    current_length = 0
+
+    for sentence in sentences:
+        if current_length + len(sentence) <= chunk_size:
+            current_chunk.append(sentence)
+            current_length += len(sentence)
+        else:
+            chunks.append(" ".join(current_chunk))
+
+            current_chunk = (
+                current_chunk[-overlap_sentences:]
+                if len(current_chunk) >= overlap_sentences
+                else current_chunk
+            )
+
+            current_length = sum(len(s) for s in current_chunk)
+
+            current_chunk.append(sentence)
+            current_length += len(sentence)
+
+    if current_chunk:
+        chunks.append(" ".join(current_chunk))
 
     return chunks
